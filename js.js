22dd6e1000af49b72f22efea5a502bfcd3c2b14c
James Woods <122772824+JamesWoods704@users.noreply.github.com>
2023-03-17 14:13:26 -0400

    btns created

--- js.js
+++ js.js
@@ -10,3 +10,20 @@ function multiply(a, b) {
 function divide(a, b) {
     a / b
 }
+function operate(operator, num1, num2){
+    switch(operator){
+        case '+':
+            return add(num1, num2);
+            break;
+        case '-':
+            return subtract(num1, num2);
+            break;
+        case '*':
+            return multiply(num1, num2);
+            break;
+        case '/':
+            return divide(num1, num2);
+            break;
+    }
+
+}
